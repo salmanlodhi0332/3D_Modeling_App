56edc8392c14b5749f3a9bcb83b9d44a05ab8cf1
salmanlodhi0332 <lodhi0332@gmail.com>
2024-08-06 12:01:35 -0700

    testing

--- modeling_app/lib/MVC/view/3DviewerScreen.dart
+++ modeling_app/lib/MVC/view/3DviewerScreen.dart
@@ -3,73 +3,84 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:model_viewer_plus/model_viewer_plus.dart';
+// import 'package:flutter_3d_controller/flutter_3d_controller.dart';
+import 'package:modeling_app/components/charactorbottomsheet.dart';
+import 'package:modeling_app/components/imageBottomSheet.dart';
 import 'package:modeling_app/components/image_widget.dart';
 import 'package:modeling_app/components/spring_widget.dart';
 import 'package:modeling_app/constant/theme.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 import 'package:modeling_app/MVC/controller/maincontroller.dart';
 import 'package:screenshot/screenshot.dart';
 
-class ModelViewerScreen extends StatelessWidget {
-  ModelViewerScreen({
+class modelViewerScreen extends StatelessWidget {
+  modelViewerScreen({
     super.key,
-    required this.model,
+    required this.Model,
   });
-
-  final String model;
-  final ScreenshotController screenshotController = ScreenshotController();
-
+  final String Model;
   @override
+  final controllersProvider = Get.put(mainController());
+  ScreenshotController screenshotController = ScreenshotController();
+  exportimage() {}
   Widget build(BuildContext context) {
-    final controllersProvider = Get.put(MainController());
+    return GetBuilder<ThemeHelper>(initState: (state) async {
+      await controllersProvider.GetAllbackground();
+    }, builder: (themecontroller) {
+      return Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: Screenshot(
+          controller: screenshotController,
+          child: Stack(
+            children: [
+              Obx(() => controllersProvider.backgoundImage.value != ''
+                  ? Positioned.fill(
+                      child: ImageWidget(
+                        imageUrl: controllersProvider.backgoundImage.value,
+                        boxfit: BoxFit.cover,
+                      ),
+                    )
+                  : Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 100, 100, 100)),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: 0,
+                            right: 10.sp,
+                            child: Image.asset(
+                              'assets/images/blankfloor.png',
+                              fit: BoxFit.fitWidth,
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              // Obx(
+              //   () => controllersProvider.Model.value == ''
+              //       ? Container()
+              // ModelViewer(
+              //     src: 'assets/images/model3.glb',
+              //     alt: "A 3D model of an astronaut",
+              //     ar: true,
+              //     autoRotate: true,
+              //     cameraControls: true,
+              //   )
+              // :
+              ModelViewer(
+                src: 'assets/images/${Model}',
+                // 'assets/images/${controllersProvider.Model.value}',
+                alt: "A 3D model of an astronaut",
+                // loading:Loading.lazy ,
 
-    return GetBuilder<ThemeHelper>(
-      initState: (state) async {
-        await controllersProvider.getAllBackground();
-      },
-      builder: (themeController) {
-        return Scaffold(
-          resizeToAvoidBottomInset: true,
-          body: Screenshot(
-            controller: screenshotController,
-            child: Stack(
-              children: [
-                Obx(
-                  () => controllersProvider.backgroundImage.value != ''
-                      ? Positioned.fill(
-                          child: ImageWidget(
-                            imageUrl: controllersProvider.backgroundImage.value,
-                            boxFit: BoxFit.cover,
-                          ),
-                        )
-                      : Container(
-                          height: double.infinity,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 100, 100, 100),
-                          ),
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                bottom: 0,
-                                right: 10.sp,
-                                child: Image.asset(
-                                  'assets/images/blankfloor.png',
-                                  fit: BoxFit.fitWidth,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                ),
-                ModelViewer(
-                  src: 'assets/images/$model',
-                  alt: "A 3D model",
-                  ar: false,
-                  autoRotate: true,
-                  cameraControls: true,
-                ),
-                Positioned(
+                ar: false,
+                autoRotate: true,
+                cameraControls: true,
+              ),
+
+              Positioned(
                   top: 30.sp,
                   right: 20.sp,
                   child: SpringWidget(
@@ -85,45 +96,65 @@ class ModelViewerScreen extends StatelessWidget {
                         size: 20.sp,
                       ),
                     ),
-                  ),
-                ),
-                Positioned(
+                  )),
+              Positioned(
                   top: 30.sp,
                   right: 80.sp,
                   child: SpringWidget(
                     onTap: () {
-                      screenshotController.capture().then((capturedImage) {
-                        if (capturedImage != null) {
-                          ShowCapturedWidget(context, capturedImage);
-                        }
+                      var container = Container(
+                          padding: const EdgeInsets.all(30.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.blueAccent, width: 5.0),
+                            color: Colors.redAccent,
+                          ),
+                          child: Stack(
+                            children: [
+                              Image.asset(
+                                'assets/images/flutter.png',
+                              ),
+                              Text(
+                                "This is an invisible widget",
+                                style: Theme.of(context).textTheme.titleLarge,
+                              ),
+                            ],
+                          ));
+                      screenshotController
+                          .captureFromWidget(
+                              InheritedTheme.captureAll(
+                                  context, Material(child: container)),
+                              delay: Duration(seconds: 1))
+                          .then((capturedImage) {
+                        ShowCapturedWidget(context, capturedImage);
                       });
                     },
                     child: CircleAvatar(
                       radius: 25.sp,
                       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
                       child: Icon(
-                        Icons.camera_alt,
+                        Icons.person,
                         color: Colors.black,
                         size: 20.sp,
                       ),
                     ),
-                  ),
-                ),
-              ],
-            ),
+                  )),
+            ],
           ),
-        );
-      },
-    );
+        ),
+      );
+    });
   }
 }
 
-Future<void> ShowCapturedWidget(BuildContext context, Uint8List capturedImage) {
+Future<dynamic> ShowCapturedWidget(
+    BuildContext context, Uint8List capturedImage) {
   return showDialog(
+    useSafeArea: false,
     context: context,
     builder: (context) => Scaffold(
       appBar: AppBar(
-        title: Text("Captured Screenshot"),
+        title: Text("Captured widget screenshot"),
       ),
       body: Center(child: Image.memory(capturedImage)),
     ),
